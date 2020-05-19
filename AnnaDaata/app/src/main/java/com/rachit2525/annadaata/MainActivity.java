ed104ac28956d295616b052e01d03fc048ea7a8f
Rachit Awasthi <rawasthi2525@gmail.com>
2020-05-19 08:01:59 +0530

    welcome screen done

--- AnnaDaata/app/src/main/java/com/rachit2525/annadaata/MainActivity.java
+++ AnnaDaata/app/src/main/java/com/rachit2525/annadaata/MainActivity.java
@@ -2,13 +2,41 @@ package com.rachit2525.annadaata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnSignIn,btnSignUp;
+    TextView txtSlogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/NABILA.TTF");
+        txtSlogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
